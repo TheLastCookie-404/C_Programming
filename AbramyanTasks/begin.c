77f7bd05ef001cea1dadf732cb649aa62ef9cdc1
The Last Cookie <se660745@gmail.com>
2024-01-11 14:02:36 +0600

    Lol

--- AbramyanTasks/begin.c
+++ AbramyanTasks/begin.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 void main()
 {
-    
+    // // Begin 1
+    // int a;
+    // scanf("%i", &a);
+    // int p = a * 4;
+    // printf("Begin 1 = %i\n", p);
+
+    // // Begin 2
+    // int a = 6;
+    // int s;
+    // s = a * a;
+    // printf("Begin 2 = %i", p);
 }
--- Playground/playground.c
+++ Playground/playground.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+
 void main()
 {
-    printf("Дароу");
+    int a = 12;
+    int *p = &a;
+    printf("Адрес = %p \nЗнаchение %i \n", (void *) p, *p);
+
+    int *b = malloc(sizeof(int));
+    *b = 123;
+    printf("Адрес = %p \nЗнаchение %i \n", (void *) b, *b);
+    free(b);
+    printf("Адрес = %p \nЗнаchение %i \n", (void *) b, *b);
 }

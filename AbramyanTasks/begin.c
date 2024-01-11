96f3f213a5b2d052b7c01e7bfafc4026872da292
The Last Cookie <se660745@gmail.com>
2024-01-11 21:45:57 +0600

    Pooooweeeeeer

--- AbramyanTasks/begin.c
+++ AbramyanTasks/begin.c
@@ -4,33 +4,56 @@
 
 void main()
 {
-    // // Begin 1
+    // Begin 1
     // int p, a = 0;
     // scanf("%i", &a);
     // p = a * 4;
     // printf("Begin 1: P = %i\n", p);
 
-    // // Begin 2
+    // Begin 2
     // int s, a = 0;
     // scanf("%i", &a);
     // s = a * a;
-    // printf("Begin 2: S = %i", s);
+    // printf("Begin 2: S = %i \n", s);
 
-    // // Begin 3
+    // Begin 3
     // int a = 0, b = 0, s = 0, p = 0;
     // scanf("%i", &a);
     // scanf("%i", &b);
     // s = a * b;
     // p = 2 * (a + b);
-    // printf("Begin 3: S = %i, P = %i", s, p);
+    // printf("Begin 3: S = %i, P = %i \n", s, p);
 
-    // // Begin 4
+    // Begin 4
     // int d = 0, l = 0;
     // const float pi = 3.14f;
     // scanf("%i", &d);
     // l = pi * d;
-    // printf("Begin 4: L = %i", l);
+    // printf("Begin 4: L = %i \n", l);
     
     // Begin 5
-    int a = 0, v = 0;
+    // int a = 0, v = 0, s = 0;
+    // scanf("%i", &a);
+    // v = a * a * a;
+    // s = 6 * a * a;
+    // printf("Begin 5: V = %i, S = %i", v, s);
+
+    // Begin 6
+    // int a = 0, b = 0, c = 0, v = 0, s = 0;
+    // scanf("%i", &a);
+    // scanf("%i", &b);
+    // scanf("%i", &c);
+    // v = a * b * c;
+    // s = 2 * (a * b + b * c + a * c);
+    // printf("Begin 6: V = %i, S = %i", v, s);
+
+    // Begin 7
+    // int l = 0, s = 0, r = 0;
+    // const float pi = 3.14f;
+    // scanf("%i", &r);
+    // l = 2 * pi * r;
+    // s = pi * (r * r);
+    // printf("Begin 7: L = %i S = %i", l, s);
+
+
 }

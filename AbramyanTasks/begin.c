17060e676bf847a3ecbba6e0b2b4bb58879f8afe
The Last Cookie <se660745@gmail.com>
2024-01-11 19:01:47 +0600

    Lol

--- AbramyanTasks/begin.c
+++ AbramyanTasks/begin.c
@@ -5,14 +5,32 @@
 void main()
 {
     // // Begin 1
-    // int a;
+    // int p, a = 0;
     // scanf("%i", &a);
-    // int p = a * 4;
-    // printf("Begin 1 = %i\n", p);
+    // p = a * 4;
+    // printf("Begin 1: P = %i\n", p);
 
     // // Begin 2
-    // int a = 6;
-    // int s;
+    // int s, a = 0;
+    // scanf("%i", &a);
     // s = a * a;
-    // printf("Begin 2 = %i", p);
+    // printf("Begin 2: S = %i", s);
+
+    // // Begin 3
+    // int a = 0, b = 0, s = 0, p = 0;
+    // scanf("%i", &a);
+    // scanf("%i", &b);
+    // s = a * b;
+    // p = 2 * (a + b);
+    // printf("Begin 3: S = %i, P = %i", s, p);
+
+    // // Begin 4
+    // int d = 0, l = 0;
+    // const float pi = 3.14f;
+    // scanf("%i", &d);
+    // l = pi * d;
+    // printf("Begin 4: L = %i", l);
+    
+    // Begin 5
+    int a = 0, v = 0;
 }

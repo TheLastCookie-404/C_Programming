4e8268a251708e8d05a70a7cb41c89faf7c792a3
The Last Cookie <se660745@gmail.com>
2024-01-17 13:23:02 +0600

    10task

--- AbramyanTasks/begin.c
+++ AbramyanTasks/begin.c
@@ -6,73 +6,80 @@
 void main()
 {
     // Begin 1
-    // int p, a = 0;
-    // scanf("%i", &a);
+    // float p, a = 0;
+    // scanf("%f", &a);
     // p = a * 4;
-    // printf("Begin 1: P = %i\n", p);
+    // printf("Begin 1: \nP = %.2f \n", p);
 
     // Begin 2
-    // int s, a = 0;
-    // scanf("%i", &a);
+    // float s, a = 0;
+    // scanf("%f", &a);
     // s = a * a;
-    // printf("Begin 2: S = %i \n", s);
+    // printf("Begin 2: \nS = %.2f \n", s);
 
     // Begin 3
-    // int a = 0, b = 0, s = 0, p = 0;
-    // scanf("%i", &a);
-    // scanf("%i", &b);
-    // s = a * b;
-    // p = 2 * (a + b);
-    // printf("Begin 3: S = %i, P = %i \n", s, p);
-
-    // Begin 4
-    // int d = 0, l = 0;
-    // const float pi = 3.14f;
-    // scanf("%i", &d);
-    // l = pi * d;
-    // printf("Begin 4: L = %i \n", l);
-    
-    // Begin 5
-    // int a = 0, v = 0, s = 0;
-    // scanf("%i", &a);
-    // v = a * a * a;
-    // s = 6 * a * a;
-    // printf("Begin 5: V = %i, S = %i", v, s);
-
-    // Begin 6
-    // int a = 0, b = 0, c = 0, v = 0, s = 0;
-    // scanf("%i", &a);
-    // scanf("%i", &b);
-    // scanf("%i", &c);
-    // v = a * b * c;
-    // s = 2 * (a * b + b * c + a * c);
-    // printf("Begin 6: V = %i, S = %i", v, s);
-
-    // Begin 7
-    // int l = 0, s = 0, r = 0;
-    // const float pi = 3.14f;
-    // scanf("%i", &r);
-    // l = 2 * pi * r;
-    // s = pi * (r * r);
-    // printf("Begin 7: L = %i S = %i", l, s);
-
-    // Begin 8
-    // int a = 0, b = 0, arithmeticMean = 0;
-    // scanf("%i", &a);
-    // scanf("%i", &b);
-    // arithmeticMean = (a + b) / 2;
-    // printf("Begin 8: Arithmetical Middle = %i \n", arithmeticMean);
-
-    // Begin 9
-    // unsigned float a = 0, b = 0;
+    // float a = 0, b = 0, s = 0, p = 0;
     // scanf("%f", &a);
     // scanf("%f", &b);
-    // printf("Begin 9: Geometrical Middle = %lf \n", sqrtf(a * b));
+    // s = a * b;
+    // p = 2 * (a + b);
+    // printf("Begin 3: \nS = %.2f, P = %.2f \n", s, p);
+
+    // Begin 4
+    // float d = 0, l = 0;
+    // const float pi = 3.14f;
+    // scanf("%f", &d);
+    // l = pi * d;
+    // printf("Begin 4: \nL = %.2f \n", l);
+    
+    // Begin 5
+    // float a = 0, v = 0, s = 0;
+    // scanf("%f", &a);
+    // v = a * a * a;
+    // s = 6 * a * a;
+    // printf("Begin 5: \nV = %.2f, S = %.2f", v, s);
+
+    // Begin 6
+    // float a = 0, b = 0, c = 0, v = 0, s = 0;
+    // scanf("%f", &a);
+    // scanf("%f", &b);
+    // scanf("%f", &c);
+    // v = a * b * c;
+    // s = 2 * (a * b + b * c + a * c);
+    // printf("Begin 6: \nV = %.2f, S = %.2f", v, s);
+
+    // Begin 7
+    // float l = 0, s = 0, r = 0;
+    // const float pi = 3.14f;
+    // scanf("%f", &r);
+    // l = 2 * pi * r;
+    // s = pi * (r * r);
+    // printf("Begin 7: \nL = %.2f S = %.2f", l, s);
+
+    // Begin 8
+    // float a = 0, b = 0;
+    // scanf("%f", &a);
+    // scanf("%f", &b);
+    // printf("egin 8: \nArithmetical Middle = %.2f \n", (a + b) / 2);
+
+    // Begin 9
+    // float a = 0, b = 0;
+    // scanf("%f", &a);
+    // scanf("%f", &b);
+    // printf("Begin 9: \nGeometrical Middle = %.2f \n", sqrtf(a * b));
 
     // Begin 10
-    int a = 0, b = 0;
-    scanf("%f", &a);
-    scanf("%f", &b);
+    // float a = 0, b = 0;
+    // float sqrSumm = 0, sqrDiffnce = 0, sqrProduct = 0,sqrQuotient = 0;
+    // scanf("%f", &a);
+    // scanf("%f", &b);
+    // sqrSumm = powf(a, 2) + powf(b, 2);
+    // sqrDiffnce = powf(a, 2) - powf(b, 2);
+    // sqrProduct = powf(a, 2) * powf(b, 2);
+    // sqrQuotient = powf(a, 2) / powf(b, 2);
+    // printf("Begin 9: \nSquare summ = %.2f \nSquare difference = %.2f \nSquare product = %.2f \nSquare quotient = %.2f \n", sqrSumm, sqrDiffnce, sqrProduct, sqrQuotient);
     
+    // Begin 11
+    // float a = 0 ,b = 0;
 
 }
